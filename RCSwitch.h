d020701c04d1ae18784c391cf96bfb9ecae3c9fc
krotof1a <christophe.safra@gmail.com>
2014-12-18 09:15:26 +0100

    Add new larger table for DIO purposes

--- RCSwitch.h
+++ RCSwitch.h
@@ -49,7 +49,7 @@ typedef uint8_t byte;
 // Number of maximum High/Low changes per packet.
 // We can handle up to (unsigned long) => 32 bit * 2 H/L changes per bit + 2 for sync
 #define RCSWITCH_MAX_CHANGES 67
-
+#define DIO_MAX_CHANGES 140
 
 class RCSwitch {
 
@@ -117,7 +117,7 @@ class RCSwitch {
 	static unsigned int nReceivedDelay;
 	static unsigned int nReceivedProtocol;
     static unsigned int timings[RCSWITCH_MAX_CHANGES];
-
+    static unsigned int timingsD[DIO_MAX_CHANGES];
     
 };
 
