8d2cc1919e5ad0c426646b8c615301196acdfefe
krotof1a <christophe.safra@laposte.net>
2014-12-23 19:23:41 +0100

    Addscript to restart wifi if down

---
+++ checkWifi.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+ping -c2 www.google.com > /dev/null
+ 
+if [ $? != 0 ] 
+then
+  echo "No network connection, restarting wlan0"
+  /sbin/ifdown 'wlan0'
+  sleep 5
+  /sbin/ifup --force 'wlan0'
+fi
