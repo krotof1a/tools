a63b375a49843d9b2c1f93fb8ddef4839d636dde
krotof1a <christophe.safra@laposte.net>
2015-02-11 17:25:17 +0100

    Bug correction

--- alarme.sh
+++ alarme.sh
@@ -3,5 +3,9 @@
 /home/pi/tools/speakCS.sh "Attention! Intrusion en cours!"
 while [ true ]
 do
-  aplay /home/pi/tools/sirene.wav
+  aplay /home/pi/tools/siren.wav > /dev/zero 2>&1
+  if [ $? -ne 0 ]
+  then
+	break
+  fi
 done
