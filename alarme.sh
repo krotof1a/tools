1b1c305657cba33ec03075b3686c2defcb8a13f7
krotof1a <christophe.safra@laposte.net>
2015-02-21 09:25:32 +0100

    Modif alarm script

--- alarme.sh
+++ alarme.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
-/home/osmc/tools/speakCS.sh "Attention! Intrusion en cours!"
-while [ true ]
-do
-  aplay /home/osmc/tools/siren.wav > /dev/zero 2>&1
-  if [ $? -ne 0 ]
-  then
-	break
-  fi
-done
+if [ "$1" = "stop" ]
+then
+	echo "Stopping running alarm ..."
+	sudo killall aplay alarme.sh
+else
+	echo "Starting alarm ..."
+	sudo /home/osmc/tools/speakCS.sh "Attention\! Intrusion en cours!"
+	while [ true ]
+	do
+  		sudo aplay /home/osmc/tools/siren.wav > /dev/zero 2>&1
+	done
+fi
