dc8029275834b4bc9368d1213fce9843aeb9acb0
krotof1a <christophe.safra@gmail.com>
2015-03-04 09:23:24 +0100

    Code simplification

--- alarme.sh
+++ alarme.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
 
-if [ "$1" = "stop" ]
-then
-	echo "Stopping running alarm ..."
-	sudo killall aplay alarme.sh
-else if [ "$1" = "start" ]
+if [ "$1" = "start" ]
 then
 	echo "Starting alarm ..."
 	sleep 10
@@ -23,8 +19,7 @@ else if [ "$1" = "finish" ]
 then
 	echo "Disarming alarm ..."
 	wget -o /dev/zero -O /dev/zero "http://localhost:5665/json.htm?type=command&param=setsecstatus&secstatus=0&seccode=$2"
-	/home/osmc/tools/alarme.sh stop
-fi
+	sudo killall aplay alarme.sh
 fi
 fi
 fi
