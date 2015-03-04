07bda73318043bf2859a8065ddc713a5cec66668
krotof1a <christophe.safra@gmail.com>
2015-03-04 09:10:57 +0100

    Enrich alarm UC

--- alarme.sh
+++ alarme.sh
@@ -4,11 +4,27 @@ if [ "$1" = "stop" ]
 then
 	echo "Stopping running alarm ..."
 	sudo killall aplay alarme.sh
-else
+else if [ "$1" = "start" ]
+then
 	echo "Starting alarm ..."
-	sudo tts "Attention\! Intrusion en cours!"
+	sleep 10
+	sudo tts "Attention\! Intrusion en cours\!"
 	while [ true ]
 	do
   		sudo aplay /home/osmc/tools/siren.wav > /dev/zero 2>&1
 	done
+else if [ "$1" = "init" ]
+then
+	echo "Arming alarm ..."
+	sudo tts "Attention\! Alarme en fonction\! Plus que dix secondes pour fermer les issues ..."
+	sleep 10
+	wget -o /dev/zero -O /dev/zero "http://localhost:5665/json.htm?type=command&param=setsecstatus&secstatus=2&seccode=$2"
+else if [ "$1" = "finish" ]
+then
+	echo "Disarming alarm ..."
+	wget -o /dev/zero -O /dev/zero "http://localhost:5665/json.htm?type=command&param=setsecstatus&secstatus=0&seccode=$2"
+	sudo killall aplay alarme.sh
+fi
+fi
+fi
 fi
