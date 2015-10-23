36b46e7fa636d4066b2f768f064e91c774734e5f
krotof1a <christophe.safra@laposte.net>
2015-10-23 22:59:49 +0200

    Replace tts by wav

--- alarme.sh
+++ alarme.sh
@@ -11,16 +11,16 @@ then
 		echo "Starting alarm ..."
 		sleep 10
 		amixer cset numid=3 1
-		sudo tts "Attention\! Intrusion en cours\!"
+		sudo aplay /home/pi/tools/intrusion.wav > /dev/zero 2>&1
 		while [ true ]
 		do
-	  		sudo aplay /home/osmc/tools/siren.wav > /dev/zero 2>&1
+	  		sudo aplay /home/pi/tools/siren.wav > /dev/zero 2>&1
 		done
 	fi
 else if [ "$1" = "init" ]
 then
 	echo "Arming alarm ..."
-	sudo tts "Attention\! Alarme en fonction\! Plus que dix secondes pour fermer les issues ..."
+	sudo aplay /home/pi/tools/arming.wav > /dev/zero 2>&1
 	sleep 10
 	wget -o /dev/zero -O /dev/zero "http://localhost:5665/json.htm?type=command&param=setsecstatus&secstatus=2&seccode=$2"
 else if [ "$1" = "finish" ]
