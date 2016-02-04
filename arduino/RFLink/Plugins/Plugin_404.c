a7107bab08c697b058cd6a45265d2a2888bf11c9
krotof1a <christophe.safra@gmail.com>
2016-02-04 13:26:13 +0100

    Update Plugin_404.c

--- arduino/RFLink/Plugins/Plugin_404.c
+++ arduino/RFLink/Plugins/Plugin_404.c
@@ -3,9 +3,7 @@
 //##                                       Plugin-404: TempCS                                          ##
 //#######################################################################################################
 /*********************************************************************************************\
- * This plugin takes care of receiving from and transmitting to "Klik-Aan-Klik-Uit" devices
- * working according to the learning code system. This protocol is also used by the Home Easy devices.
- * It includes direct DIM functionality.
+ * This plugin takes care of receiving from and transmitting to RCSwitch Protocol 1 devices
  * 
  * Author             : StuntTeam
  * Support            : http://sourceforge.net/projects/rflink/
@@ -13,12 +11,11 @@
  *                      Usage of any parts of this code in a commercial application is prohibited!
  ***********************************************************************************************
  \*********************************************************************************************/
-#define TempCS_RawSignalLength       36        // regular KAKU packet length
+#define TempCS_RawSignalLength       36        // regular TempCS packet length
 #define TempCS_mT                   600/RAWSIGNAL_SAMPLE_RATE // us, approx. in between 1T and 4T 
 
 #ifdef PLUGIN_404
 boolean Plugin_404(byte function, char *string) {
-      // nieuwe KAKU bestaat altijd uit start bit + 32 bits + evt 4 dim bits. Ongelijk, dan geen NewKAKU
       if (RawSignal.Number != TempCS_RawSignalLength) return false;
       boolean Bit;
       int i=1;
@@ -29,9 +26,9 @@ boolean Plugin_404(byte function, char *string) {
           P1=RawSignal.Pulses[i+1]; // * RawSignal.Multiply;
           
           if (P0<TempCS_mT && P1>TempCS_mT) {  
-              Bit=0; // T,T,T,4T
+              Bit=0; // short-long
           } else if (P0>TempCS_mT && P1<TempCS_mT) {
-              Bit=1; // T,4T,T,T
+              Bit=1; // long-short
           }
           bitstream=(bitstream<<1) | Bit;     
           i+=2;                                                    // Next 2 pulses
@@ -52,12 +49,12 @@ boolean Plugin_404(byte function, char *string) {
       sprintf(pbuffer, "20;%02X;", PKSequenceNumber++);    // Node and packet number 
       Serial.print( pbuffer );
       // ----------------------------------
-      Serial.print("TempCS;");                         // Label
+      Serial.print("TempCS;");                            // Label
       sprintf(pbuffer, "ID=%01x;",((bitstream) & 15) );   // ID   
       Serial.print( pbuffer );
       int positive = ((bitstream) >> 1) & 1;
       int temperature = ((bitstream) >> 5) / 10;
-      if (positive == 0) temperature = temperature * -1;
+      if (positive == 0) temperature = temperature * -1;  // to be reworked
       sprintf(pbuffer, "TEMP=%04x;", temperature);
       Serial.print( pbuffer );
       Serial.println();
