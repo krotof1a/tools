9a39a5d3250915c4535bc61b8ed6fc60cee132d5
krotof1a <christophe.safra@laposte.net>
2017-01-23 12:54:40 +0100

    Modify API key

--- shutdownOctoPi.sh
+++ shutdownOctoPi.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-curl -s -H "Content-Type: application/json" -H "X-Api-Key: A28235311C5C44459F7ADF1FBAA15DF5" -X POST -d '{ "command":"connect" }' http://192.168.1.33/api/system/commands/core/shutdown
+curl -s -H "Content-Type: application/json" -H "X-Api-Key: 123CEDB65F504A7997870642936020B6" -X POST -d '{ "command":"connect" }' http://192.168.1.33/api/system/commands/core/shutdown
