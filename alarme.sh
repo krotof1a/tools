2723bec0bec2c9123c4544837032976362f9a71d
krotof1a <christophe.safra@laposte.net>
2017-01-24 12:40:28 +0100

    Adapt to chip

--- alarme.sh
+++ alarme.sh
@@ -7,26 +7,26 @@ then
 	then
 		echo "Alarm already running ... Nothing to do."
 	else
-		sudo touch $RUN
+		touch $RUN
 		echo "Starting alarm ..."
 		sleep 10
 		#amixer cset numid=3 1
 		while [ true ]
 		do
-	  		sudo aplay /home/pi/tools/siren.wav > /dev/zero 2>&1
+	  		aplay /home/chip/tools-domo/siren.wav > /dev/zero 2>&1
 		done
 	fi
 else if [ "$1" = "init" ]
 then
 	echo "Arming alarm ..."
 	sleep 10
-	wget -o /dev/zero -O /dev/zero "http://localhost:5665/json.htm?type=command&param=setsecstatus&secstatus=2&seccode=$2"
+	wget -o /dev/zero -O /dev/zero "http://127.0.0.1:5665/json.htm?type=command&param=setsecstatus&secstatus=2&seccode=$2"
 else if [ "$1" = "finish" ]
 then
 	echo "Disarming alarm ..."
-	sudo \rm -f $RUN
-	wget -o /dev/zero -O /dev/zero "http://localhost:5665/json.htm?type=command&param=setsecstatus&secstatus=0&seccode=$2"
-	sudo killall aplay alarme.sh
+	\rm -f $RUN
+	wget -o /dev/zero -O /dev/zero "http://127.0.0.1:5665/json.htm?type=command&param=setsecstatus&secstatus=0&seccode=$2"
+	killall aplay alarme.sh
 fi
 fi
 fi
