0deeef9640bb99a02ddef5ac2e8c01cf082b013f
krotof1a <christophe.safra@laposte.net>
2017-07-02 12:59:29 +0200

    Add control of RTL mode

---
+++ rtlMode.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+if [ $1 = "0" ]
+then
+	echo "systemctl stop dump1090" | nc -q 1 192.168.1.22 60000
+	echo "systemctl stop rtl_tcp" | nc -q 1 192.168.1.22 60000
+else if [ $1 = "10" ]
+then
+	echo "systemctl stop dump1090" | nc -q 1 192.168.1.22 60000
+	echo "systemctl start rtl_tcp" | nc -q 1 192.168.1.22 60000
+else if [ $1 = "20" ]
+then
+	echo "systemctl stop rtl_tcp" | nc -q 1 192.168.1.22 60000
+	echo "systemctl start dump1090" | nc -q 1 192.168.1.22 60000
+fi
+fi
+fi
+
