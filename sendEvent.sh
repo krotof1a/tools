fc220c8c388c7b83270d6351215b08522e8a2035
krotof1a <christophe.safra@laposte.net>
2017-08-25 11:02:33 +0200

    Some improvemnts

--- sendEvent.sh
+++ sendEvent.sh
@@ -5,17 +5,28 @@ port="5665" # port Domoticz
 idx_planning="284" # idx de l'interrupteur Planning
 jour_sem=256
 emiter=2063597568
-
+phase=1
 mycmd=0
+
+if [ $1 = "15" ]
+then
+	echo "reboot"
+	mycmd=15728640
+	timer=0
+        fullCmd=$(expr $emiter + $mycmd + $timer)
+	/home/pi/build/433Utils/RPi_utils/codesend $fullCmd 1
+	exit 0
+fi
 if [ $1 = "1" ]
 then
 	echo "On"
 	mycmd=1048576
+	phase=0
 fi
 
 date_serveur=$(curl -s "http://$ip:$port/json.htm?type=command&param=getSunRiseSet" | jq -r '.ServerTime | strptime("%Y-%m-%d %H:%M:%S") | mktime')
 # On recupere le prochain timer sur le planning (parfois le mauvais mais avec le bon jour)
-timer=$(curl -s "http://$ip:$port/json.htm?type=schedules" | jq '[.result[] | select((.DeviceRowID | contains ('$idx_planning')) and ((.Days | contains('$jour_sem')) or (.Days | contains(128))) and (.TimerCmd | contains('$1')))]' | jq '[.[].ScheduleDate | strptime("%Y-%m-%d %H:%M:%S") | mktime]' | jq 'min')
+timer=$(curl -s "http://$ip:$port/json.htm?type=schedules" | jq '[.result[] | select((.DeviceRowID | contains ('$idx_planning')) and ((.Days | contains('$jour_sem')) or (.Days | contains(128))) and (.TimerCmd | contains('$phase')))]' | jq '[.[].ScheduleDate | strptime("%Y-%m-%d %H:%M:%S") | mktime]' | jq 'min')
 if [ $(date --date=@$timer +%u) -gt 5 ]
    then
    jour_sem=512
@@ -23,7 +34,7 @@ else
    jour_sem=256
 fi
 # On relance la requete avec le bon jour
-timer=$(curl -s "http://$ip:$port/json.htm?type=schedules" | jq '[.result[] | select((.DeviceRowID | contains ('$idx_planning')) and ((.Days | contains('$jour_sem')) or (.Days | contains(128))) and (.TimerCmd | contains('$1')))]' | jq '[.[].ScheduleDate | strptime("%Y-%m-%d %H:%M:%S") | mktime]' | jq 'min')
+timer=$(curl -s "http://$ip:$port/json.htm?type=schedules" | jq '[.result[] | select((.DeviceRowID | contains ('$idx_planning')) and ((.Days | contains('$jour_sem')) or (.Days | contains(128))) and (.TimerCmd | contains('$phase')))]' | jq '[.[].ScheduleDate | strptime("%Y-%m-%d %H:%M:%S") | mktime]' | jq 'min')
 timer=$(expr $timer - $date_serveur)
 echo "Timestamp prochain timer : $timer"
 fullCmd=$(expr $emiter + $mycmd + $timer)
--- sendSync.sh
+++ sendSync.sh
@@ -3,12 +3,13 @@
 ip="192.168.1.20" # ip locale
 port="5665" # port Domoticz
 idx_planning="284" # idx de l'interrupteur Planning
-
+idx_synccing="482" # idx de l'interrupteur Synccing
 date_serveur=$(curl -s "http://$ip:$port/json.htm?type=command&param=getSunRiseSet" | jq -r '.ServerTime | strptime("%Y-%m-%d %H:%M:%S") | mktime')
 /home/pi/build/433Utils/RPi_utils/codesend $date_serveur 1
-sleep 1
+curl -s "http://$ip:$port/json.htm?type=command&param=switchlight&idx=$idx_synccing&switchcmd=Off"
+sleep 5
 
-/home/pi/tools-domo/sendEvent.sh 1
-sleep 1
+curl -s "http://$ip:$port/json.htm?type=command&param=switchlight&idx=$idx_planning&switchcmd=Toggle"
+sleep 5
 
-/home/pi/tools-domo/sendEvent.sh 0
+curl -s "http://$ip:$port/json.htm?type=command&param=switchlight&idx=$idx_planning&switchcmd=Toggle"
