044dd703a0b934d419d212580625cfc33dd2446d
krotof1a <christophe.safra@laposte.net>
2017-10-23 17:07:09 +0200

    Bug correction

--- pioneer.py
+++ pioneer.py
@@ -8,16 +8,16 @@ if sys.argv[1] == 'on':
 elif sys.argv[1] == 'off':
 	tn.write("PF\r\n")
 elif sys.argv[1] == 'vol':
-	goal = int(sys.argv[2]) * 1.5
+	goal = int(round(int(sys.argv[2]) * 1.5))
 	tn.write("?V\r\n")
 	output = tn.read_until("\r\n")
 	currentlevel = int(output[3:])
-	if goal < 150: # my set upper limit to prevent damage to speakers
+	if goal < 150:
   		if currentlevel > goal:
-    			for x in range(currentlevel, goal, -1):
+    			for x in range(currentlevel, goal, -2):
       				tn.write("VD\r\n")
   		else:
-    			for x in range(currentlevel, goal, 1):
+    			for x in range(currentlevel, goal, 2):
       				tn.write("VU\r\n")
 	else:
   		sys.stdout.write('Given voulume is out of range\n')
