be233f012090713e99a31b02e2526e7051d2e488
krotof1a <christophe.safra@laposte.net>
2019-02-03 08:22:04 +0100

    change ip

--- rtlMode.sh
+++ rtlMode.sh
@@ -2,19 +2,19 @@
 
 if [ $1 = "0" ]
 then
-	echo "systemctl stop dump1090-mutability" | nc -q 1 192.168.1.23 60000
+	echo "systemctl stop dump1090-mutability" | nc -q 1 192.168.1.98 60000
 	sleep 3
-	echo "systemctl stop rtl_tcp" | nc -q 1 192.168.1.23 60000
+	echo "systemctl stop rtl_tcp" | nc -q 1 192.168.1.98 60000
 else if [ $1 = "10" ]
 then
-	echo "systemctl stop dump1090-mutability" | nc -q 1 192.168.1.23 60000
+	echo "systemctl stop dump1090-mutability" | nc -q 1 192.168.1.98 60000
 	sleep 3
-	echo "systemctl start rtl_tcp" | nc -q 1 192.168.1.23 60000
+	echo "systemctl start rtl_tcp" | nc -q 1 192.168.1.98 60000
 else if [ $1 = "20" ]
 then
-	echo "systemctl stop rtl_tcp" | nc -q 1 192.168.1.23 60000
+	echo "systemctl stop rtl_tcp" | nc -q 1 192.168.1.98 60000
 	sleep 3
-	echo "systemctl start dump1090-mutability" | nc -q 1 192.168.1.23 60000
+	echo "systemctl start dump1090-mutability" | nc -q 1 192.168.1.98 60000
 fi
 fi
 fi
